8453afb61de19e5479698296626c13fb4deaedca
Axyl07 <aryanchauhan000777@gmail.com>
2024-08-11 10:14:57 +0530

    done

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let input = string;
+    let ans = '';
+    for (let index = input.length-1; index >= 0; index--) {
+         ans += input.charAt(index);
+    }
+    return ans;
 };
 
 // Do not edit below this line
