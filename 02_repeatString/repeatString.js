202a67903e492c5db73649be945ba73a4d2809d0
Axyl07 <aryanchauhan000777@gmail.com>
2024-08-11 09:48:24 +0530

    Done

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,17 @@
-const repeatString = function() {
-
+const repeatString = function(string , num) {
+        if (num<0) {
+            return 'ERROR';
+        }
+        let input = string;
+        let ans = "";
+        for (let index = 0; index < num; index++) {
+            ans+=input;
+        }    
+     
+        return ans; 
+            
+       
+       
 };
 
 // Do not edit below this line
