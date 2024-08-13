ee6fef65312fc996a25685828ba5657bb375aae8
Axyl07 <aryanchauhan000777@gmail.com>
2024-08-13 06:42:01 +0530

    done

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,21 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...args) {
+    //args is an array
+    let size  = args.length;
+    for (let index = 0; index < array.length; index++) {
+        let val = args[0];
+        if (array[index]===val) {
+            array.splice(index,1);
+        }
+        
+    }
+    for (let i = 0; i < size; i++) {   
+        let value = args[i];
+        let pos = array.indexOf(value);
+        if (pos>-1) {
+            array.splice(pos,1);          
+        }
+}   
+    return array;
 };
 
 // Do not edit below this line
